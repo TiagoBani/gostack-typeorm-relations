6515f91732d365c812b9f18509efab4760b128d2
tiagobani <tiago.bani@yahoo.com.br>
2020-10-09 07:12:34 -0300

    [feat]: add annotations in entities

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,15 +6,21 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('customers')
 class Customer {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column({ name: 'name' })
   name: string;
 
+  @Column({ name: 'email' })
   email: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
